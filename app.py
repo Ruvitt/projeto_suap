eeb2001ba707797274e2dd7162a414367d168b2d
Ruan Cordeiro <ruan59333@gmail.com>
2023-12-08 22:06:28 -0300

    interface, busca automatica por anos letivos, vizualizar boletim de outros anos

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, session, request, jsonify, render_template
 from authlib.integrations.flask_client import OAuth
+from config import client_id, client_secret
 
 app = Flask(__name__)
 app.debug = True
@@ -8,8 +9,8 @@ oauth = OAuth(app)
 
 oauth.register(
     name='suap',
-    client_id= '',
-    client_secret= '',
+    client_id= client_id,
+    client_secret= client_secret,
     api_base_url='https://suap.ifrn.edu.br/api/',
     request_token_url=None,
     access_token_method='POST',
@@ -18,18 +19,39 @@ oauth.register(
     fetch_token=lambda: session.get('suap_token')
 )
 
-@app.route('/')
-def index():
+def obter_anos_letivos():
+    periodo_data = oauth.suap.get('/api/v2/minhas-informacoes/meus-periodos-letivos/')
+    anos_letivos = [periodo['ano_letivo'] for periodo in periodo_data.json()]	
+    anos_letivos = list(dict.fromkeys(anos_letivos))
+
+    return anos_letivos
+
+def obter_boletim(ano_letivo):
     if 'suap_token' in session:
-        ano_letivo = 2023
-        periodo_letivo = 1
+        periodo_letivo = 1 
+
+        boletim_response = oauth.suap.get(f'/api/v2/minhas-informacoes/boletim/{ano_letivo}/{periodo_letivo}/')
+        return boletim_response.json()
+
+    return None
+
+@app.route('/')
+@app.route('/<ano_letivo>/')
+def index(ano_letivo = '2023'):
+    if 'suap_token' in session:
+        anos_letivos = obter_anos_letivos()
+        ano_letivo_selecionado = ano_letivo
+
         user_data = oauth.suap.get('v2/minhas-informacoes/meus-dados')
-        boletim_data = oauth.suap.get(f'/api/v2/minhas-informacoes/boletim/{ano_letivo}/{periodo_letivo}/')
-        print(user_data.json())
-        print(boletim_data.json()) 
-        return render_template('userdata.html', user_data=user_data.json(), boletim_data=boletim_data.json())
+        boletim_data = obter_boletim(ano_letivo_selecionado)
+        return render_template('userdata.html', user_data=user_data.json(), boletim_data=boletim_data, anos_letivos=anos_letivos, ano_letivo_selecionado=ano_letivo_selecionado)
     else:
         return render_template('index.html')
+    
+@app.route('/boletim', methods=['POST'])
+def atualizar_boletim():
+    ano_letivo = request.form.get('ano_letivo')
+    return redirect(url_for('index', ano_letivo=ano_letivo))
     
 @app.route('/login')
 def login():
@@ -40,7 +62,6 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('suap_token', None)
-    session.pop('ano_letivo', None)
     return redirect(url_for('index'))
 
 @app.route('/login/authorized')
